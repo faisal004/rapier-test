3477269d72e9998b994ad6599947c42043233d3c
Faisal Husain <faisalhusain1320@gmail.com>
2025-03-30 21:31:35 +0530

    first

---
+++ src/Experience.jsx
@@ -0,0 +1,35 @@
+import { OrbitControls } from '@react-three/drei'
+import { Perf } from 'r3f-perf'
+import { Physics, RigidBody, CuboidCollider } from "@react-three/rapier";
+
+export default function Experience() {
+    return <>
+
+        <Perf position="top-left" />
+
+        <OrbitControls makeDefault />
+
+        <directionalLight castShadow position={[1, 2, 3]} intensity={4.5} />
+        <ambientLight intensity={1.5} />
+        <Physics debug>
+            <RigidBody type='dynamic'>
+            <mesh castShadow position={[- 2, 2, 0]}>
+                <sphereGeometry />
+                <meshStandardMaterial color="orange" />
+            </mesh>
+            </RigidBody>
+
+            <mesh castShadow position={[2, 2, 0]}>
+                <boxGeometry />
+                <meshStandardMaterial color="mediumpurple" />
+            </mesh>
+            <RigidBody type='fixed'>
+
+            <mesh receiveShadow position-y={- 1.25}>
+                <boxGeometry args={[10, 0.5, 10]} />
+                <meshStandardMaterial color="greenyellow" />
+            </mesh>
+            </RigidBody>
+        </Physics>
+    </>
+}
